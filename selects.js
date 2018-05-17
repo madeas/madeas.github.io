28df3b1909bfca1f0fab974665afab9732dd20f2
Andrej Sharapov <an.sharapov90@gmail.com>
2018-05-17 11:36:08 +0300

    selects.js

--- selects.js
+++ selects.js
@@ -1,5 +1,17 @@
+[].forEach.call(document.querySelectorAll('.block'), function(node) {
+	node.onmouseover = function() {
+	    document.querySelector('.card__content').style.boxShadow = getComputedStyle(node).boxShadow;
+        }
+	node.onmouseleave = function() {
+	    document.querySelector('.card__content').style.boxShadow = "";
+        }
+});
 document.querySelector('select').onchange = function() {
 	var node = document.querySelector("." + this.value);
 	    document.querySelector('.card__content').style.boxShadow = getComputedStyle(node).boxShadow;
  
 };
+[].forEach.call(document.querySelectorAll(''), function(node) {
+	var bsh = getComputedStyle(node).boxShadow;
+	node.style.boxShadow = "inset "+bsh;
+});
