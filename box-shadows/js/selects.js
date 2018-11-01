48859e50c0e8dd17edc044b0af8aef4e2406e4bd
Andrej Sharapov <an.sharapov90@gmail.com>
2018-11-01 14:35:51 +0300

    .js

--- box-shadows/js/selects.js
+++ box-shadows/js/selects.js
@@ -12,7 +12,7 @@ document.querySelector('select').onchange = function() {
 	var node = document.querySelector("." + this.value);
 	    document.querySelector('.card__content').style.boxShadow = getComputedStyle(node).boxShadow; 
 };
-[].forEach.call(document.querySelectorAll(''), function(node) {
-	var bsh = getComputedStyle(node).boxShadow;
-	node.style.boxShadow = "inset "+bsh;
-});
+// [].forEach.call(document.querySelectorAll(''), function(node) {
+// 	var bsh = getComputedStyle(node).boxShadow;
+// 	node.style.boxShadow = "inset "+bsh;
+// });
